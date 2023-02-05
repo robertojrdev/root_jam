dde0d2eba3f2ee5f055b3e77b1040a3461e1cfa1
joaosdev <joaosdev@gmail.com>
2023-02-05 03:00:21 +0000

    - po crl diogo eu dou o nome q eu quiser aos meus commits

---
+++ GGJ23_Root/Assets/WalkerRunnerManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkerRunnerManager : MonoBehaviour
+{
+    [Header("State")]
+    public bool runner = true;
+    public bool walker = false;
+
+    //[Header("Transition")]
+    //[Range(0,1)]
+    //public float transitionSlider = 0f;
+
+    [Header("Components")]
+    public WalkerCamera wCam;
+    public WalkerMovement wMove;
+    //public Camera cam;
+
+    public void StartWalker()
+    {
+        runner = false;
+        walker = true;
+
+        wCam.Init();
+    }
+
+    //private void Update()
+    //{
+    //}
+}
